14cbae3990913acbe607fa8e5282e55564d0be22
fenroe <scrambles9494@gmail.com>
2024-12-19 22:30:42 +0000

    can resize images

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import (
 	"fmt"
 	"image"
-	_ "image/jpeg"
-	_ "image/png"
+	"image/jpeg"
 	"os"
+
+	"golang.org/x/image/draw"
 )
 
 func getImageFromFilePath(filepath string) (image.Image, error) {
@@ -18,6 +19,26 @@ func getImageFromFilePath(filepath string) (image.Image, error) {
 	return image, err
 }
 
+func resizeImage(img image.Image) *image.RGBA {
+	rgba := image.NewRGBA(image.Rect(0, 0, img.Bounds().Max.X/2, img.Bounds().Max.Y/2))
+	draw.CatmullRom.Scale(rgba, rgba.Rect, img, img.Bounds(), draw.Over, nil)
+	return rgba
+}
+
+func saveImage(path string, img *image.RGBA) error {
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	err = jpeg.Encode(file, img, nil)
+	return err
+}
+
+func getPercentage(x, y int) float64 {
+	return float64((y / x) * 100)
+}
+
 func main() {
 	fmt.Println("Hi banana!")
 	wd, err := os.Getwd()
@@ -25,4 +46,24 @@ func main() {
 		return
 	}
 	fmt.Println(wd)
+	args := os.Args[1:]
+	path := args[0]
+	image, err := getImageFromFilePath(path)
+	if err != nil {
+		fmt.Println("Oh no!")
+		return
+	}
+	fmt.Printf("width:%v, height:%v\n", image.Bounds().Dx(), image.Bounds().Dy())
+	resizedImage := resizeImage(image)
+	err = saveImage(path, resizedImage)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	image, err = getImageFromFilePath(path)
+	if err != nil {
+		fmt.Println("Oh no!")
+		return
+	}
+	fmt.Printf("width:%v, height:%v\n", image.Bounds().Dx(), image.Bounds().Dy())
 }
