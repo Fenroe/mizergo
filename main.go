fac961330a10fcdb505ba640f9a065333f650513
fenroe <scrambles9494@gmail.com>
2024-12-26 22:59:18 +0000

    added some structure to folders

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/jpeg"
 	"os"
+	"strconv"
 
 	"golang.org/x/image/draw"
 )
+
+type settings struct {
+	width               int64
+	height              int64
+	maintainAspectRatio bool
+	path                string
+	output              string
+	
+}
 
 func getImageFromFilePath(filepath string) (image.Image, error) {
 	file, err := os.Open(filepath)
@@ -39,8 +50,54 @@ func getPercentage(x, y int) float64 {
 	return float64((y / x) * 100)
 }
 
+func getSettings(args []string) (settings, error) {
+	settings := settings{
+		width:               0,
+		height:              0,
+		maintainAspectRatio: true,
+		path:                "",
+	}
+
+	for i, arg := range args {
+		switch arg {
+		case "-w", "--w", "-width", "--width":
+			width, err := strconv.ParseInt(args[i+1], 10, 64)
+			if err != nil {
+				return settings, err
+			}
+			settings.width = width
+		case "-h", "--h", "-height", "--height":
+			height, err := strconv.ParseInt(args[i+1], 10, 64)
+			if err != nil {
+				return settings, err
+			}
+			settings.height = height
+		case "-ar", "--ar", "-aspect", "--aspect":
+			maintainAspectRatio, err := strconv.ParseBool(args[i+1])
+			if err != nil {
+				return settings, err
+			}
+			settings.maintainAspectRatio = maintainAspectRatio
+		case "-p", "--p", "-path", "--path":
+			if len(args) <= i+1 {
+				return settings, errors.New("path flag provided without appropriate value")
+			}
+			path := args[i+1]
+			settings.path = path
+		case "-o", "--o", "-output", "--output":
+			if len(args) <= i+1 {
+				return settings, errors.New("output flag provided without appropriate value")
+			}
+			output := args[i+1]
+			settings.output = output
+		default:
+			continue
+		}
+	}
+	return settings, nil
+}
+
 func main() {
-	fmt.Println("Hi banana!")
 	wd, err := os.Getwd()
 	if err != nil {
 		return
